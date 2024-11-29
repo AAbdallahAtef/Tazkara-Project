fd21b58bcb44119a13ec5609da855799e79d3dc9
Abdallah Atef <96814163+AAbdallahAtef@users.noreply.github.com>
2024-11-29 11:31:28 +0200

    Update line of port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 // MongoDB connection setup
 mongoose.connect('mongodb+srv://abdallaatef12:Aatef2000@tazkaracluster.1hsop.mongodb.net/tazkara');
